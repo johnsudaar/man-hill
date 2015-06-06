bad79974d944fdd2604f44706f71d007c137175b
johnsudaar <jonathan.hurter@laposte.net>
2015-06-06 16:22:40 +0200

    Base Commit

---
+++ ManHill/Edge.py
@@ -0,0 +1,17 @@
+# Man Hill project
+# By Pierre Collet, Damien Theodori and Jonathan Hurter
+
+# Edge.py
+
+class Edge(object):
+
+    def __init__(self):
+
+    def loadFromJson(self,json):
+
+    def toJson(self):
+
+    def setValue(pheromone, pos,neg):
+
+    def getAllPheromones():
+    
---
+++ ManHill/Matrix.py
@@ -0,0 +1,18 @@
+# Man Hill project
+# By Pierre Collet, Damien Theodori and Jonathan Hurter
+
+# Edge.py
+
+class Matrix(object):
+
+    def __init__(self):
+
+    def loadFromJson(self,json):
+
+    def toJson(self):
+
+    def getNodeIn(self, node):
+
+    def getNodeOut(self, node):
+
+    def set(self, in,out,pheromone,val_pos, val_neg):
